ac1d4e7549f27f9ad4a6763a997608ceb4a453c3
sultonbayevogabek <sultonbayevogabek@gmail.com>
2025-09-19 19:01:34 +0500

    initial commit

--- js/register.js
+++ js/register.js
@@ -2,12 +2,6 @@
 
 (async _ => {
   let user = JSON.parse(localStorage.getItem('user'));
-
-  const joinButton = document.querySelector('#joinToChannel')
-  joinButton?.addEventListener('click', async (e) => {
-    window.location.href = 'https://t.me/+gNGqJioywn4zYzli';
-  })
-
   if (user && user?.name && user?.phone && user?.time) {
     const formData = new FormData();
 
@@ -15,7 +9,7 @@
     formData.append('Telefon raqami', user?.phone);
     formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
 
-    let response = await fetch('https://script.google.com/macros/s/AKfycbyV2b_CW3DwoJK9u-IYRjewq1K-WPyi8uW2nHTfVX2_lIoVi3xFilbJa79qDHV5A13p/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbwCMxZ4RUlQr79hYgMne9kjpOFmQAU_pmTwPT4ycyhwnhtNcKsKNivcVyGfTgt0rsZU/exec', {
       method: 'POST',
       body: formData
     })
